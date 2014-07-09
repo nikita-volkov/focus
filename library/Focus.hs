1a19cf52556594817be6f913425d5ed2d14417ed
Nikita Volkov <nikita.y.volkov@mail.ru>
2014-07-09 16:48:53 +0400

    INLINE pragmas

--- library/Focus.hs
+++ library/Focus.hs
@@ -32,36 +32,42 @@ data Decision a =
 -- |
 -- Reproduces the behaviour of
 -- @Data.Map.<http://hackage.haskell.org/package/containers-0.5.5.1/docs/Data-Map-Lazy.html#v:adjust adjust>@.
+{-# INLINE adjust #-}
 adjust :: (a -> a) -> Strategy a ()
 adjust f = maybe ((), Keep) (\a -> ((), Replace (f a)))
 
 -- |
 -- Reproduces the behaviour of
 -- @Data.Map.<http://hackage.haskell.org/package/containers-0.5.5.1/docs/Data-Map-Lazy.html#v:update update>@.
+{-# INLINE update #-}
 update :: (a -> Maybe a) -> Strategy a ()
 update f = maybe ((), Keep) (\a -> ((), maybe Remove Replace (f a)))
 
 -- |
 -- Reproduces the behaviour of
 -- @Data.Map.<http://hackage.haskell.org/package/containers-0.5.5.1/docs/Data-Map-Lazy.html#v:alter alter>@.
+{-# INLINE alter #-}
 alter :: (Maybe a -> Maybe a) -> Strategy a ()
 alter f = ((),) . maybe Remove Replace . f
 
 -- |
 -- Reproduces the behaviour of
 -- @Data.Map.<http://hackage.haskell.org/package/containers-0.5.5.1/docs/Data-Map-Lazy.html#v:insert insert>@.
+{-# INLINE insert #-}
 insert :: a -> Strategy a ()
 insert a = const ((), Replace a)
 
 -- |
 -- Reproduces the behaviour of
 -- @Data.Map.<http://hackage.haskell.org/package/containers-0.5.5.1/docs/Data-Map-Lazy.html#v:delete delete>@.
+{-# INLINE delete #-}
 delete :: Strategy a ()
 delete = const ((), Remove)
 
 -- |
 -- Reproduces the behaviour of
 -- @Data.Map.<http://hackage.haskell.org/package/containers-0.5.5.1/docs/Data-Map-Lazy.html#v:lookup lookup>@.
+{-# INLINE lookup #-}
 lookup :: Strategy a (Maybe a)
 lookup r = (r, Keep)
 
@@ -71,31 +77,37 @@ lookup r = (r, Keep)
 
 -- |
 -- A monadic version of 'adjust'.
+{-# INLINE adjustM #-}
 adjustM :: (Monad m) => (a -> m a) -> StrategyM m a ()
 adjustM f = maybe (return ((), Keep)) (liftM (((),) . Replace) . f)
 
 -- |
 -- A monadic version of 'update'.
+{-# INLINE updateM #-}
 updateM :: (Monad m) => (a -> m (Maybe a)) -> StrategyM m a ()
 updateM f = maybe (return ((), Keep)) (liftM (((),) . maybe Remove Replace) . f)
 
 -- |
 -- A monadic version of 'alter'.
+{-# INLINE alterM #-}
 alterM :: (Monad m) => (Maybe a -> m (Maybe a)) -> StrategyM m a ()
 alterM f = liftM (((),) . maybe Remove Replace) . f
 
 -- |
 -- A monadic version of 'insert'.
+{-# INLINE insertM #-}
 insertM :: (Monad m) => a -> StrategyM m a ()
 insertM = fmap return . insert
 
 -- |
 -- A monadic version of 'delete'.
+{-# INLINE deleteM #-}
 deleteM :: (Monad m) => StrategyM m a ()
 deleteM = fmap return delete
 
 -- |
 -- A monadic version of 'lookup'.
+{-# INLINE lookupM #-}
 lookupM :: (Monad m) => StrategyM m a (Maybe a)
 lookupM = fmap return lookup
 
