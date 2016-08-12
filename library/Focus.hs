07777ee6bb7d0c07fe1fd03ad04df11c95ff2385
Nikita Volkov <nikita.y.volkov@mail.ru>
2016-08-13 02:38:10 +0300

    Rename Strategy to Focus

--- library/Focus.hs
+++ library/Focus.hs
@@ -10,11 +10,11 @@ import Control.Monad
 -- a 'Decision' to perform on the match.
 -- 
 -- The interpretation of this function is up to the context APIs.
-type Strategy a r = Maybe a -> (r, Decision a)
+type Focus a r = Maybe a -> (r, Decision a)
 
 -- |
--- A monadic version of 'Strategy'.
-type StrategyM m a r = Maybe a -> m (r, Decision a)
+-- A monadic version of 'Focus'.
+type FocusM m a r = Maybe a -> m (r, Decision a)
 
 -- |
 -- What to do with the focused value.
@@ -34,42 +34,42 @@ data Decision a =
 -- Reproduces the behaviour of
 -- @Data.Map.<http://hackage.haskell.org/package/containers-0.5.5.1/docs/Data-Map-Lazy.html#v:adjust adjust>@.
 {-# INLINE adjust #-}
-adjust :: (a -> a) -> Strategy a ()
+adjust :: (a -> a) -> Focus a ()
 adjust f = maybe ((), Keep) (\a -> ((), Replace (f a)))
 
 -- |
 -- Reproduces the behaviour of
 -- @Data.Map.<http://hackage.haskell.org/package/containers-0.5.5.1/docs/Data-Map-Lazy.html#v:update update>@.
 {-# INLINE update #-}
-update :: (a -> Maybe a) -> Strategy a ()
+update :: (a -> Maybe a) -> Focus a ()
 update f = maybe ((), Keep) (\a -> ((), maybe Remove Replace (f a)))
 
 -- |
 -- Reproduces the behaviour of
 -- @Data.Map.<http://hackage.haskell.org/package/containers-0.5.5.1/docs/Data-Map-Lazy.html#v:alter alter>@.
 {-# INLINE alter #-}
-alter :: (Maybe a -> Maybe a) -> Strategy a ()
+alter :: (Maybe a -> Maybe a) -> Focus a ()
 alter f = ((),) . maybe Remove Replace . f
 
 -- |
 -- Reproduces the behaviour of
 -- @Data.Map.<http://hackage.haskell.org/package/containers-0.5.5.1/docs/Data-Map-Lazy.html#v:insert insert>@.
 {-# INLINE insert #-}
-insert :: a -> Strategy a ()
+insert :: a -> Focus a ()
 insert a = const ((), Replace a)
 
 -- |
 -- Reproduces the behaviour of
 -- @Data.Map.<http://hackage.haskell.org/package/containers-0.5.5.1/docs/Data-Map-Lazy.html#v:delete delete>@.
 {-# INLINE delete #-}
-delete :: Strategy a ()
+delete :: Focus a ()
 delete = const ((), Remove)
 
 -- |
 -- Reproduces the behaviour of
 -- @Data.Map.<http://hackage.haskell.org/package/containers-0.5.5.1/docs/Data-Map-Lazy.html#v:lookup lookup>@.
 {-# INLINE lookup #-}
-lookup :: Strategy a (Maybe a)
+lookup :: Focus a (Maybe a)
 lookup r = (r, Keep)
 
 
@@ -79,37 +79,37 @@ lookup r = (r, Keep)
 -- |
 -- A monadic version of 'adjust'.
 {-# INLINE adjustM #-}
-adjustM :: (Monad m) => (a -> m a) -> StrategyM m a ()
+adjustM :: (Monad m) => (a -> m a) -> FocusM m a ()
 adjustM f = maybe (return ((), Keep)) (liftM (((),) . Replace) . f)
 
 -- |
 -- A monadic version of 'update'.
 {-# INLINE updateM #-}
-updateM :: (Monad m) => (a -> m (Maybe a)) -> StrategyM m a ()
+updateM :: (Monad m) => (a -> m (Maybe a)) -> FocusM m a ()
 updateM f = maybe (return ((), Keep)) (liftM (((),) . maybe Remove Replace) . f)
 
 -- |
 -- A monadic version of 'alter'.
 {-# INLINE alterM #-}
-alterM :: (Monad m) => (Maybe a -> m (Maybe a)) -> StrategyM m a ()
+alterM :: (Monad m) => (Maybe a -> m (Maybe a)) -> FocusM m a ()
 alterM f = liftM (((),) . maybe Remove Replace) . f
 
 -- |
 -- A monadic version of 'insert'.
 {-# INLINE insertM #-}
-insertM :: (Monad m) => a -> StrategyM m a ()
+insertM :: (Monad m) => a -> FocusM m a ()
 insertM = fmap return . insert
 
 -- |
 -- A monadic version of 'delete'.
 {-# INLINE deleteM #-}
-deleteM :: (Monad m) => StrategyM m a ()
+deleteM :: (Monad m) => FocusM m a ()
 deleteM = fmap return delete
 
 -- |
 -- A monadic version of 'lookup'.
 {-# INLINE lookupM #-}
-lookupM :: (Monad m) => StrategyM m a (Maybe a)
+lookupM :: (Monad m) => FocusM m a (Maybe a)
 lookupM = fmap return lookup
 
 
