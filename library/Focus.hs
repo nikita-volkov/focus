60c051f4fd1f75e0e2fe170c73168d29de22f3c0
Nikita Volkov <nikita.y.volkov@mail.ru>
2018-07-17 18:02:49 +0300

    Refactor and add more functions

--- library/Focus.hs
+++ library/Focus.hs
@@ -16,33 +16,95 @@ newtype Focus element m result = Focus (Maybe element -> m (result, Maybe elemen
 deriving instance Functor m => Functor (Focus element m)
 
 instance Monad m => Applicative (Focus element m) where
-  pure result = Focus (\ state -> pure (result, state))
+  pure = return
   (<*>) = ap
 
 instance Monad m => Monad (Focus element m) where
-  return = pure
+  return result = Focus (\ state -> return (result, state))
   (>>=) (Focus focusA) bKleisli =
     Focus $ \ state -> do
       (aResult, aOutState) <- focusA state
       case bKleisli aResult of
         Focus focusB -> focusB aOutState
 
+prim :: Monad m => (Maybe a -> (b, Maybe a)) -> Focus a m b
+prim fn = Focus (return . fn)
+
+{-|
+Reproduces the behaviour of
+@Data.Map.<http://hackage.haskell.org/package/containers-0.6.0.1/docs/Data-Map-Lazy.html#v:insert insert>@.
+-}
+{-# INLINE insert #-}
+insert :: Monad m => a -> Focus a m ()
+insert a = alter (const (Just a))
+
+{-|
+Reproduces the behaviour of
+@Data.Map.<http://hackage.haskell.org/package/containers-0.6.0.1/docs/Data-Map-Lazy.html#v:insertWith insertWith>@
+with a better name.
+-}
+insertOrMerge :: Monad m => (a -> a -> a) -> a -> Focus a m ()
+insertOrMerge merge value = alter (Just . maybe value (merge value))
+
+{-|
+Reproduces the behaviour of
+@Data.Map.<http://hackage.haskell.org/package/containers-0.6.0.1/docs/Data-Map-Lazy.html#v:alter alter>@.
+-}
+{-# INLINE alter #-}
+alter :: Monad m => (Maybe a -> Maybe a) -> Focus a m ()
+alter fn = prim (((),) . fn)
+
+{-|
+Reproduces the behaviour of
+@Data.Map.<http://hackage.haskell.org/package/containers-0.6.0.1/docs/Data-Map-Lazy.html#v:adjust adjust>@.
+-}
+{-# INLINE adjust #-}
 adjust :: Monad m => (a -> a) -> Focus a m ()
-adjust fn = Focus (pure . ((),) . fmap fn)
+adjust fn = alter (fmap fn)
 
+{-|
+Reproduces the behaviour of
+@Data.Map.<http://hackage.haskell.org/package/containers-0.6.0.1/docs/Data-Map-Lazy.html#v:update update>@.
+-}
+{-# INLINE update #-}
 update :: Monad m => (a -> Maybe a) -> Focus a m ()
-update fn = Focus (pure . ((),) . flip (>>=) fn)
+update fn = alter (flip (>>=) fn)
 
+{-|
+Reproduces the behaviour of
+@Data.Map.<http://hackage.haskell.org/package/containers-0.6.0.1/docs/Data-Map-Lazy.html#v:lookup lookup>@.
+-}
 {-# INLINE[1] lookup #-}
 lookup :: Monad m => Focus a m (Maybe a)
-lookup = Focus (pure . (id &&& id))
+lookup = prim (id &&& id)
 
+{-|
+Reproduces the behaviour of
+@Data.Map.<http://hackage.haskell.org/package/containers-0.6.0.1/docs/Data-Map-Lazy.html#v:findWithDefault findWithDefault>@
+with a better name.
+-}
+{-# INLINE[1] lookupWithDefault #-}
+lookupWithDefault :: Monad m => a -> Focus a m a
+lookupWithDefault a = prim (maybe a id &&& id)
+
+{-|
+Reproduces the behaviour of
+@Data.Map.<http://hackage.haskell.org/package/containers-0.6.0.1/docs/Data-Map-Lazy.html#v:lookup lookup>@.
+-}
+{-# INLINE member #-}
+member :: Monad m => Focus a m Bool
+member = fmap (maybe False (const True)) lookup
+
+{-|
+Reproduces the behaviour of
+@Data.Map.<http://hackage.haskell.org/package/containers-0.6.0.1/docs/Data-Map-Lazy.html#v:delete delete>@.
+-}
 {-# INLINE[1] delete #-}
 delete :: Monad m => Focus a m ()
-delete = Focus (const (pure ((), Nothing)))
+delete = alter (const Nothing)
 
 {-# RULES
   "lookup <* delete" [~1] lookup <* delete = lookupAndDelete
   #-}
 lookupAndDelete :: Monad m => Focus a m (Maybe a)
-lookupAndDelete = Focus (\ state -> pure (state, Nothing))
+lookupAndDelete = prim (\ state -> (state, Nothing))
